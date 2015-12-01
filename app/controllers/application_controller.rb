3d7cec0e5db54e87b16e1044ec132b2d1ca36854
James <JamesCarlson2@hotmail.com>
2015-12-01 09:53:52 -0600

    Include devise controller conditional for paramater sanitizing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  before_action :configure_permitted_parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     user_path(current_user)
